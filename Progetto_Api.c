b6c107738aef97dfbd2b48df4d1e99ba4f0fdccb
A1liceCariboni <alice.cariboni@mail.polimi.it>
2021-08-01 19:07:52 +0200

    da sistemare max heap

--- Progetto_Api.c
+++ Progetto_Api.c
@@ -3,100 +3,160 @@
 #include<math.h>
 #include<string.h>
 #include<stdlib.h>
+#include<stdbool.h>
 #define MAX_DIST -1
 
-struct vertex_from0{
-int num, dist;
+
+struct vertex_from0 {
+int num;
+long long dist;
+bool visited;
+};
+
+struct topK{
+	int graph;
+	long long weight;
 };
 //riga = j/d;
 //colonna = j%d
+
 
 // j = d * riga + colonna
 
 
 
-int k, d, size, heapsize;
+int k, d, size, graphcount = 0, ranksize = 0;
 
-//costruisce min heap
-void min_heapify(struct vertex_from0 vert[], int n){
-int dx, sx, min;
-struct vertex_from0 v;
+
+
+//costruisce max heap
+void max_heapify(struct topK rank[], int n){
+int dx, sx, max;
+struct topK r;
    sx = 2 * n;
    dx = (2 * n) + 1;
-   if((sx < heapsize)&&(vert[sx].dist < vert[n].dist)){
-   		  min = sx;
+   if((sx < ranksize)&&(rank[sx].weight > rank[n].weight)){
+   		  max = sx;
 	}else{
-		min = n;
+		max = n;
 	}
-	if((dx < heapsize) && (vert[dx].dist < vert[n].dist)){
-		min = dx;
+	if((dx < ranksize) && (rank[dx].weight > rank[n].weight)){
+		max = dx;
 	}
-	if(min != n){
-	v = vert[min];
-	vert[min] = vert[n];
-	vert[n] = v;
-	min_heapify(vert, min);	     	  	  
+	if(max != n){
+	r = rank[max];
+	rank[max] = rank[n];
+	rank[n] = r;
+	max_heapify(rank, max);	     	  	  
 	}
+}
+
+void place_in_rank(struct topK rank[], long long sum){
+	struct topK r;
+	int i;
+	r.graph = graphcount;
+	r.weight = sum;
+	
+	if(ranksize < k){
+		rank[ranksize] = r;
+		ranksize ++;
+	 	i = ranksize - 1;
+		 while((i > 0)	&& (rank[i/2].weight < rank[i].weight)){
+		 	r = rank[i/2];
+		 	rank[i/2] = rank[i];
+		 	rank[i] = r;
+		 	i = i/2;
+		 }
+	}else{
+		if(sum < rank[0].weight){
+			rank[0] = rank[ranksize - 1];
+			ranksize --;
+			max_heapify(rank, 0);
+			rank[ranksize] = r;
+			ranksize ++;
+		 	i = ranksize - 1;
+			 while((i > 0)	&& (rank[i/2].weight < rank[i].weight)){
+			 	r = rank[i/2];
+			 	rank[i/2] = rank[i];
+			 	rank[i] = r;
+			 	i = i/2;
+			 }
+		}
+	}
+}
+
+void print_rank(struct topK rank[]){
+	int i;
+	for(i = 0; i < ranksize - 1; i++){
+		printf("%d ", rank[i].graph);
+	}
+        printf("%d\n", rank[ranksize - 1].graph);
 
 }
+
 
 //restituisce il minimo e sistema lo heap
 struct vertex_from0 get_min(struct vertex_from0 vert[]){
-	struct vertex_from0 min = vert[0];
+	struct vertex_from0 min;
+	int i;
 	
-	vert[0] = vert[heapsize - 1];
-	heapsize--;
-	min_heapify(vert,0);
-	vert[heapsize] = min;
+	min.dist = 9223372036854775807;
+	min.num = -1;
+	
+	for(i = 0; i < d; i++){
+		if((!vert[i].visited) && (vert[i].dist < min.dist) && (vert[i].dist != -1)){
+			min = vert[i];
+		}
+	}
+	
 	return min;
 }
 
-long int djkstra(int graph[]){
- struct vertex_from0 vertex[d], u;
- int start, i, j;
- heapsize = d;
- int dist;
- long int sum_dis;
- 
- vertex[0].dist = 0;
- vertex[0].num = 0;
-	for(i = 1; i < d; i++){
-		vertex[i].dist = INT_MAX;
-		vertex[i].num = i;
-	}
+long long djkstra(long long graph[]){
+	 struct vertex_from0 vertex[d], u;
+	 int start, i;
+	 long long dist;
+	 long long sum_dis;
 	 
- for(i = d/2 - 1; i >= 0; i--){
- 	min_heapify(vertex, i);
- }
- 
- while(heapsize > 0){
-	u = get_min(vertex);
-	start = d * u.num;
-	for(i = start + 1; i < start + d; i++){
-		if((graph[i] != 0)){
-		 dist = u.dist + graph[i];
-		for(j = 0; j < heapsize; j++){
-		  if((vertex[j].num == i % d)&&(vertex[j].dist > dist)){
-		  	vertex[j].dist = dist;
-		  	min_heapify(vertex, 0);
-		  }	
-  		}
-  	  }
-	}
- }
- sum_dis = 0;
-for(i = 0; i < d; i++){
-	sum_dis = sum_dis + vertex[i].dist;
-}
-return sum_dis;	 
+	 vertex[0].dist = 0;
+	 vertex[0].num = 0;
+	 vertex[0].visited = false;
+		for(i = 1; i < d; i++){
+			vertex[i].dist = MAX_DIST;
+			vertex[i].num = i;
+			vertex[i].visited = false;
+		}
+		 
+	 
+	 	
+	 sum_dis = 0; 	
+	 u = get_min(vertex);  
+	 while(u.num != -1){
+		vertex[u.num].visited = true;
+	    sum_dis = sum_dis + vertex[u.num].dist;
+	
+		start = d * u.num;
+		for(i = start; i < start + d; i++){
+			if(graph[i] != 0){
+				dist = u.dist + graph[i];
+			if((!vertex[i%d].visited)&&((vertex[i % d].dist > dist)||(vertex[i % d].dist == -1))){
+				  	vertex[i%d].dist = dist;
+				  }	
+		  		}
+	    }
+  	    u = get_min(vertex);
+   }
+	 
+	return sum_dis;	 
 }
 
 
 int main(){
 char str[11], c;
-int *graph;
+long long *graph;
+struct topK *rank;
 int i, j;
-long int sum;
+long long sum;
 i = 0;
 c = fgetc(stdin);
 while(c != ' '){
@@ -117,7 +177,9 @@ while(c != '\n'){
 str[i] = '\0';
 k = atoi(str);
 
-graph = malloc(sizeof(int)*size);
+graph = malloc(sizeof(long long)*size);
+rank = malloc(sizeof(struct topK)*k);
+
 c = fgetc(stdin);
 while(c != EOF){
   	if(c == 'A'){
@@ -134,17 +196,16 @@ while(c != EOF){
 			  str[i] = '\0';
 			  graph[j] = atol(str);
 	    }
- 		printf("%d\n", k);
-  		printf("%d\n", d);
-  		for(j = 0; j < size; j ++){
-  			  printf("%d ", graph[j]);
-		  }
-    sum = djkstra(graph);
-    printf("\n%ld", sum);
-	  }
-    else{
-    	
-		 }
+    	sum = djkstra(graph);
+    	place_in_rank(rank, sum);
+    	printf("GraphCount = %d\n Sum = %lld\n",graphcount, sum);
+    	print_rank(rank);
+ 		graphcount++;
+
+    }else{
+    	while(c != '\n') c = fgetc(stdin);
+    	print_rank(rank);
+        }
 		 c = fgetc(stdin);
 
   
